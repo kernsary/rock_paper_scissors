04652fa690a7706723fe4dfa1604c581bd6c0fdd
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-01 17:56:17 +0100

    further extension with class variable

--- adv rock_paper_scissors/controller.rb
+++ adv rock_paper_scissors/controller.rb
@@ -5,14 +5,16 @@ require('pry')
 require_relative('./models/rock_paper_scissors')
 also_reload('./models/*')
 
+@@hand1 = nil
+
 get '/player1/:hand1' do
-  $hand1 = params[:hand1]
+  @@hand1 = params[:hand1]
   return nil
 end
 
 get '/player2/:hand2' do
   @hand2 = params[:hand2]
-  @outcome = RockPaperScissors.play($hand1, @hand2)
+  @outcome = RockPaperScissors.play(@@hand1, @hand2)
   erb(:result)
 end
 
