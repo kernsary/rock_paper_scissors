4f777fe17721be6812ac9323f50a385cf56a9e80
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-01 19:57:29 +0100

    further extension fwith class variable in method

---
+++ adv2_rock_paper_scissors/models/rock_paper_scissors.rb
@@ -0,0 +1,43 @@
+class RockPaperScissors
+
+  # def self.play(hand1, hand2)
+  #   valid_options = ["rock", "paper", "scissors"]
+  #   if !valid_options.include?(hand1) || !valid_options.include?(hand2)
+  #     return "Sorry, the valid options are 'rock', 'paper' and 'scissors'."
+  #   end
+  #   if hand1 == hand2
+  #     return "Draw!"
+  #   end
+  #   if (hand1 == 'rock' && hand2 == 'scissors') || (hand1 == 'scissors' && hand2 == 'paper') || (hand1 == 'paper' && hand2 == 'rock')
+  #     return "Player 1 wins!"
+  #   else
+  #     return "Player 2 wins!"
+  #   end
+  # end
+
+  # @@hand1 = nil
+
+  def self.player1(hand1)
+    valid_options = ["rock", "paper", "scissors"]
+    if !valid_options.include?(hand1)
+      return "Sorry, the valid options are 'rock', 'paper' and 'scissors'."
+    end
+    @@hand1 = hand1
+  end
+
+  def self.player2(hand2)
+    valid_options = ["rock", "paper", "scissors"]
+    if !valid_options.include?(hand2)
+      return "Sorry, the valid options are 'rock', 'paper' and 'scissors'."
+    end
+    if @@hand1 == hand2
+      return "Draw!"
+    end
+    if (@@hand1 == 'rock' && hand2 == 'scissors') || (@@hand1 == 'scissors' && hand2 == 'paper') || (@@hand1 == 'paper' && hand2 == 'rock')
+      return "Player 1 wins!"
+    else
+      return "Player 2 wins!"
+    end
+  end
+
+end
