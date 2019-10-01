be2022594d1256ac8e2c0add46fec6097cd55987
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-01 17:46:46 +0100

    further extension completed using global variable

---
+++ adv rock_paper_scissors/controller.rb
@@ -0,0 +1,31 @@
+require('sinatra')
+require('sinatra/contrib/all') if development?
+require('pry')
+
+require_relative('./models/rock_paper_scissors')
+also_reload('./models/*')
+
+get '/player1/:hand1' do
+  $hand1 = params[:hand1]
+  return nil
+end
+
+get '/player2/:hand2' do
+  @hand2 = params[:hand2]
+  @outcome = RockPaperScissors.play($hand1, @hand2)
+  erb(:result)
+end
+
+# get '/player1/:hand1' do
+#   RockPaperScissors.player1(params[:hand1])
+#   return nil
+# end
+#
+# get '/player2/:hand2' do
+#   @outcome = RockPaperScissors.player2(params{:hand2})
+#   erb(:result)
+# end
+
+get '/' do
+  erb (:welcome)
+end
